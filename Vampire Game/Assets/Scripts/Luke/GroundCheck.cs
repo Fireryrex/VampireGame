521c250e87199b32e91df3b8b8ff7b1bd1ea4532
Luke M <lukeam@uci.edu>
2019-06-25 15:37:14 -0700

    Rat movement done

---
+++ Vampire Game/Assets/Scripts/Luke/GroundCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 11)
+        {
+            GetComponentInParent<RatMovement>().turnAround();
+        }
+    }
+}
