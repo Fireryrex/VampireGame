7a8ccb2eec3166366a1d74b3e2085b642dd9bbe8
Fireryrex <victorwz@uci.edu>
2019-07-05 15:20:39 -0700

    some rat boss stuff

--- Vampire Game/Assets/Scripts/Luke/RatMovement.cs
+++ Vampire Game/Assets/Scripts/Luke/RatMovement.cs
@@ -15,6 +15,8 @@ public class RatMovement : MonoBehaviour
 
     private bool hasTurned;
 
+    private bool isGrounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,10 +36,12 @@ public class RatMovement : MonoBehaviour
         {
             turnAround();
         }
-        
 
-        rb.velocity = rb.velocity * new Vector3(0, 1, 0);
-        rb.AddForce(transform.right * Time.deltaTime * forceMultiplyer);
+        if (isGrounded)
+        {
+            rb.velocity = rb.velocity * new Vector3(0, 1, 0);
+            rb.AddForce(transform.right * Time.deltaTime * forceMultiplyer);
+        }
     }
 
     public void turnAround()
@@ -51,6 +55,14 @@ public class RatMovement : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             movingRight = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 11)
+        {
+            isGrounded = true;
         }
     }
 }
--- Vampire Game/Assets/Scripts/Rat_Horde/Rat_Wall_Trigger.cs
+++ Vampire Game/Assets/Scripts/Rat_Horde/Rat_Wall_Trigger.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Rat_Wall_Trigger : MonoBehaviour
 {
-    public Rat_Wall ratWall;
+    public Rat_Wall[] ratWall;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("TrIgGeReD");
-        ratWall.moveRatWall1();
+        for(int i = 0; i < ratWall.Length; ++i)
+        {
+            ratWall[i].moveRatWall1();
+        }
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
     }
 }
