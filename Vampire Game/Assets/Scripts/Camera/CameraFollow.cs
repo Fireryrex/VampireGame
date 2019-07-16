0bb817e42788e878805b680b0de3686db5372649
Luke M <lukeam@uci.edu>
2019-07-16 16:04:08 -0700

    got camera into a working point and added the camera hook point to the player

---
+++ Vampire Game/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+
+    public Vector3 offset;
+    public float cameraMoveSpeed = 1f;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 cameraDir = ((target.position + offset) - transform.position).normalized;
+        float distance = Vector3.Distance((target.position + offset), transform.position);
+
+
+        transform.position = (transform.position + cameraDir * distance * cameraMoveSpeed * Time.deltaTime);
+
+        
+    }
+}
