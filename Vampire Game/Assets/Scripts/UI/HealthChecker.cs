3154bccbb35b41da2b4e22a0bd6c5c0aa5022a9a
Luke M <lukeam@uci.edu>
2019-08-09 15:05:43 -0700

    me

---
+++ Vampire Game/Assets/Scripts/UI/HealthChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HealthChecker : MonoBehaviour
+{
+    public TMP_Text myText;
+    public Health_Script playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        myText.text = "Health: ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "Health: " + playerHealth.health;
+    }
+}
