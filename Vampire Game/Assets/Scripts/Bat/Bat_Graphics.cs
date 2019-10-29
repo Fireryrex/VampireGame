85514de49726f953d9b646f671861f7aeac68e34
Fireryrex <victorwz@uci.edu>
2019-10-29 15:13:07 -0700

    bat is mostly done except attack

---
+++ Vampire Game/Assets/Scripts/Bat/Bat_Graphics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat_Graphics : MonoBehaviour
+{
+    public Pathfinding.AIPath ai_path;
+
+    private void Start()
+    {
+        ai_path = GetComponentInParent<Pathfinding.AIPath>();
+    }
+    private void Update()
+    {
+        if(ai_path.desiredVelocity.x > 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (ai_path.desiredVelocity.x < 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
