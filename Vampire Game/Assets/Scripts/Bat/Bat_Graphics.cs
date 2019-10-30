0fb0695fbefb43bf70905884c870520cc317b7e1
Fireryrex <victorwz@uci.edu>
2019-10-30 11:47:31 -0700

    cool

--- Vampire Game/Assets/Scripts/Bat/Bat_Graphics.cs
+++ Vampire Game/Assets/Scripts/Bat/Bat_Graphics.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Bat_Graphics : MonoBehaviour
 {
-    public Pathfinding.AIPath ai_path;
+    private Pathfinding.AIPath ai_path;
+    private Animator bat_Animation;
 
     private void Start()
     {
+        bat_Animation = GetComponent<Animator>();
+        AnimatorStateInfo state = bat_Animation.GetCurrentAnimatorStateInfo(0);
+        bat_Animation.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
         ai_path = GetComponentInParent<Pathfinding.AIPath>();
     }
     private void Update()
