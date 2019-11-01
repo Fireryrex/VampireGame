752a0fd49a229803d163718df3f42a9d93f5bdd9
Fireryrex <victorwz@uci.edu>
2019-10-31 18:45:27 -0700

    pulling to get things

--- Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
+++ Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Bat_Attack : MonoBehaviour
 {
     private Animator bat_attack;
+    private Rigidbody2D bat_body;
+    private Pathfinding.AIDestinationSetter AI_Setter;
+    private Transform bat_target;
 
     // Start is called before the first frame update
+    //santi was here
     void Start()
     {
-        bat_attack = GetComponentInChildren<Animator>();   
+        AI_Setter = GetComponentInParent<Pathfinding.AIDestinationSetter>();
+        bat_attack = GetComponentInChildren<Animator>();
+        bat_body = GetComponentInParent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,11 +24,22 @@ public class Bat_Attack : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
+            bat_target = collision.transform;
+            //bat_body.bodyType = RigidbodyType2D.Static;
             bat_attack.SetTrigger("See_Player");
+            AI_Setter.target = null;
         }
+    }
+
+    public void endAnimation()
+    {
+        bat_attack.ResetTrigger("See_Player");
+        AI_Setter.target = bat_target;
+        //bat_body.bodyType = RigidbodyType2D.Dynamic;
+
     }
 }
