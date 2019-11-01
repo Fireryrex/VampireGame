3e8df40ffc7e94bb17a1e0cc2c9f2ca0f39d3b87
Fireryrex <victorwz@uci.edu>
2019-10-31 17:25:23 -0700

    maybe door not break

--- Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
+++ Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
@@ -23,6 +23,7 @@ public class Bat_Attack : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             bat_attack.SetTrigger("See_Player");
+            Debug.Log("Penis");
         }
     }
 }
