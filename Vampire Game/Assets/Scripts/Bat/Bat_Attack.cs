7d4ec093f41395ff2e5c1ff9be1a5afab822a712
Fireryrex <victorwz@uci.edu>
2019-10-31 17:03:42 -0700

    your fucking welcome gavin you POS

--- Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
+++ Vampire Game/Assets/Scripts/Bat/Bat_Attack.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Bat_Attack : MonoBehaviour
 {
+    private Animator bat_attack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bat_attack = GetComponentInChildren<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            bat_attack.SetTrigger("See_Player");
+        }
     }
 }
