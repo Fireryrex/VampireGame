ac2be5a684b3bb339b9d419af78be4dda312d38d
Fireryrex <victorwz@uci.edu>
2019-11-13 09:42:07 -0800

    uh oh i may have caused a fire

--- Vampire Game/Assets/Scripts/Rat_Horde/Rat_Horde_AI.cs
+++ Vampire Game/Assets/Scripts/Rat_Horde/Rat_Horde_AI.cs
@@ -391,7 +391,11 @@ I need to implement a case in the attack switch so that it can choose to activat
             else
             {   
                 platform.SetActive(true);
-                if(time > 3f)
+                if(time >= 2f)
+                {
+                    moveWarning.SetActive(true);
+                }
+                if (time >= 3f)
                 {
                     hasDove = false;
                     readyToJump = false;
