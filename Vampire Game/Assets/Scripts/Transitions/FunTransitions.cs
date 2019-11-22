bee71e20407a9f6dcb3a1d5dfc128d78b646d729
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-11-21 19:17:13 -0800

    Scene Transition

---
+++ Vampire Game/Assets/Scripts/Transitions/FunTransitions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FunTransitions : MonoBehaviour
+{
+    public string TransitionTo;
+    public GameObject FadeinQuad;
+    public float cooldown = 1;
+    float cutoff = 1;
+    public float transitionTime;
+    GameObject gamemanager;
+
+    private void Update() 
+    {
+        cooldown -= Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(cooldown <= 0 && other.gameObject.tag == "Player")
+        {
+            GameManager.instance.TransitionScene(TransitionTo,transitionTime);
+            StartCoroutine("Transition");
+        }
+    }
+
+}
