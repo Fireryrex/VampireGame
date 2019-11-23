24b3860fac7c2d3b5c30cb52a9c0822d4c890ed5
Fireryrex <victorwz@uci.edu>
2019-11-22 19:10:18 -0800

    boss scene completed

--- Vampire Game/Assets/Scripts/Rat_Horde/Rat_Wall_Trigger.cs
+++ Vampire Game/Assets/Scripts/Rat_Horde/Rat_Wall_Trigger.cs
@@ -6,10 +6,11 @@ public class Rat_Wall_Trigger : MonoBehaviour
 {
     [SerializeField] Rat_Wall[] ratWall;
     [SerializeField] Rat_Horde_AI bossAI;
+    [SerializeField] BossHealthScript bossHP;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if(collision.CompareTag("Player") && bossHP.getHealthPercent() > 0)
         {
             Debug.Log("TrIgGeReD");
             bossAI.startFight();
