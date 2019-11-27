fd628b96a73d8f80f257816dac705142569f6e70
Fireryrex <victorwz@uci.edu>
2019-11-26 20:54:50 -0800

    more bugs

--- Vampire Game/Assets/Scripts/Level Stuff/RespawnPoint.cs
+++ Vampire Game/Assets/Scripts/Level Stuff/RespawnPoint.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class RespawnPoint : MonoBehaviour
 {
+    private Animator checkpointAnimator;
+    private Vector2 respawnPosition;
+
+
+    private void Start()
+    {
+        checkpointAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (checkpointAnimator.GetBool("isActive") == false)
+        {
+            respawnPosition = gameObject.transform.position;
+            if (GameManager.instance.RespawnPoint == respawnPosition)
+            {
+                checkpointAnimator.SetBool("isActive", true);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player"))
         {
+            checkpointAnimator.SetBool("isActive", true);
             GameManager.instance.RespawnPoint = gameObject.transform.position;
             GameManager.instance.respawnLevel = SceneManager.GetActiveScene().name;
             other.GetComponent<Health_Script>().respawnPoint = SceneManager.GetActiveScene().name;
